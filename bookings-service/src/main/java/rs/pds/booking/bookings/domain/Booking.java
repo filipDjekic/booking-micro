6cd8983bd290b8c3b1f506ccc5a301f52c6f45d0
Filip Đekić <e14filipdjekic@gmail.com>
2025-10-25 21:37:34 +0200

    dodat model

--- bookings-service/src/main/java/rs/pds/booking/bookings/domain/Booking.java
+++ bookings-service/src/main/java/rs/pds/booking/bookings/domain/Booking.java
@@ -1,4 +1,74 @@
 package rs.pds.booking.bookings.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private long userId;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    @Column(nullable = false, length = 50)
+    private String resourceName;
+
+    @NotNull
+    private LocalDateTime start;
+
+    @NotNull;
+    private LocalDateTime end;
+
+    @NotNull
+    @DecimalMin(value = "0.01")
+    @Digits(integer = 8, fraction = 2)
+    private BigDecimal price;
+
+    //getter-i
+    public Long getId() {
+        return id;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public String getResourceName() {
+        return resourceName;
+    }
+    public LocalDateTime getStart() {
+        return start;
+    }
+    public LocalDateTime getEnd() {
+        return end;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    //setter-i
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
