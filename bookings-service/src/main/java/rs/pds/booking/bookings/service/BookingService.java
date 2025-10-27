f0923e28ff18be786b6869ca2167b0833642ee8a
Filip Đekić <e14filipdjekic@gmail.com>
2025-10-27 14:36:37 +0100

    problem neki sa resilience

--- bookings-service/src/main/java/rs/pds/booking/bookings/service/BookingService.java
+++ bookings-service/src/main/java/rs/pds/booking/bookings/service/BookingService.java
@@ -1,5 +1,9 @@
 package rs.pds.booking.bookings.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import rs.pds.booking.bookings.client.UserClient;
 import rs.pds.booking.bookings.domain.Booking;
@@ -10,6 +14,8 @@ import rs.pds.booking.bookings.repository.BookingRepository;
 @Service
 public class BookingService {
 
+    private static final Logger log = LoggerFactory.getLogger(BookingService.class);
+
     private final BookingRepository bookingRepository;
     private final UserClient userClient;
 
@@ -18,10 +24,22 @@ public class BookingService {
         this.userClient = userClient;
     }
 
+    @CircuitBreaker(name="usersCleint", fallbackMethod = "getDetailsFallback")
+    @Retry(name="usersClient")
     public BookingDetails getDetails(Long bookingId){
         Booking b =  bookingRepository.findById(bookingId).orElseThrow(() -> new IllegalArgumentException("Booking po ID-ju nije nadjen (ID: "+bookingId+")"));
 
         UserSummary user = userClient.getById(b.getUserId());
+
+        return new BookingDetails(b.getId(), b.getUserId(), b.getResourceName(), b.getStart(), b.getEnd(), b.getPrice(), user);
+    }
+
+    public BookingDetails getDetailsFallback(Long bookingId, Throwable ex){
+        log.warn("Fallback trigerovan za bookingId: "+bookingId+",; cause:"+(ex!=null ? ex.toString() : "n/a"));
+
+        Booking b = bookingRepository.findById(bookingId).orElseThrow(() -> new IllegalArgumentException("Booking " + bookingId + " nije pronadjen."));
+
+        UserSummary user = new UserSummary(null, "NEMA", "NeMa");
 
         return new BookingDetails(b.getId(), b.getUserId(), b.getResourceName(), b.getStart(), b.getEnd(), b.getPrice(), user);
     }
